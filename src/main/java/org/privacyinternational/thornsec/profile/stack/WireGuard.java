4f4dd8e78404bb156a1f39869f1f9f3a3af33611
Ed Geraghty <ed@geraghty.london>
2021-05-21 12:36:07 +0100

    Move the Wireguard interface into its profile - keep it in one place

--- src/main/java/org/privacyinternational/thornsec/profile/stack/WireGuard.java
+++ src/main/java/org/privacyinternational/thornsec/profile/stack/WireGuard.java
@@ -7,12 +7,10 @@
  */
 package org.privacyinternational.thornsec.profile.stack;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import javax.json.JsonObject;
-
+import org.privacyinternational.thornsec.core.data.AData;
+import org.privacyinternational.thornsec.core.data.machine.configuration.NetworkInterfaceData;
 import org.privacyinternational.thornsec.core.data.machine.configuration.TrafficRule.Encapsulation;
+import org.privacyinternational.thornsec.core.exception.data.ADataException;
 import org.privacyinternational.thornsec.core.exception.data.InvalidIPAddressException;
 import org.privacyinternational.thornsec.core.exception.data.InvalidPortException;
 import org.privacyinternational.thornsec.core.exception.data.machine.InvalidServerException;
@@ -21,11 +19,19 @@ import org.privacyinternational.thornsec.core.exception.runtime.InvalidMachineMo
 import org.privacyinternational.thornsec.core.exception.runtime.InvalidServerModelException;
 import org.privacyinternational.thornsec.core.iface.IUnit;
 import org.privacyinternational.thornsec.core.model.machine.ServerModel;
-import org.privacyinternational.thornsec.core.model.machine.configuration.networking.WireGuardModel;
+import org.privacyinternational.thornsec.core.model.machine.configuration.networking.NetworkInterfaceModel;
+import org.privacyinternational.thornsec.core.model.network.NetworkModel;
 import org.privacyinternational.thornsec.core.model.network.UserModel;
 import org.privacyinternational.thornsec.core.profile.AStructuredProfile;
 import org.privacyinternational.thornsec.core.unit.SimpleUnit;
+import org.privacyinternational.thornsec.core.unit.fs.FileUnit;
 import org.privacyinternational.thornsec.core.unit.pkg.InstalledUnit;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
 
 /**
  * This builds a WireGuard server on a given machine (intended to be run on a
@@ -33,13 +39,95 @@ import org.privacyinternational.thornsec.core.unit.pkg.InstalledUnit;
  */
 public class WireGuard extends AStructuredProfile {
 
-	private final Integer listenPort;
+	/**
+	 * This model creates a WireGuard interface through systemd-networkd.
+	 *
+	 * For more information, see https://www.wireguard.com/
+	 */
+	public class WireGuardModel extends NetworkInterfaceModel {
+
+		private Collection<UserModel> peers;
+		private Integer listenPort;
+
+		public WireGuardModel(NetworkInterfaceData myData, NetworkModel networkModel) throws InvalidNetworkInterfaceException, InvalidIPAddressException {
+			super(myData, networkModel);
+
+			setInet(NetworkInterfaceData.Inet.WIREGUARD);
+			setWeighting(20);
+		}
+
+		public void setListenPort(Integer listenPort) {
+			this.listenPort = listenPort;
+		}
+
+		@Override
+		public Optional<FileUnit> getNetDevFile() {
+			FileUnit netdev = super.getNetDevFile().get();
+
+			netdev.appendCarriageReturn();
+			netdev.appendLine("[WireGuard]");
+			netdev.appendLine("PrivateKey=$(cat /etc/wireguard/private.key)");
+			netdev.appendLine("ListenPort=" + this.listenPort);
+
+			this.peers.forEach((peer) -> {
+				netdev.appendCarriageReturn();
+				netdev.appendLine("[WireGuardPeer]");
+				netdev.appendLine("PublicKey=" + peer.getWireGuardKey().orElseGet(() -> ""));
+				netdev.appendLine("PresharedKey=" + peer.getWireguardPSK().orElseGet(() -> ""));
+				netdev.appendLine("AllowedIPs=" + String.join(", ", peer.getWireGuardIPs().orElseGet(() -> new ArrayList<>())));
+				netdev.appendLine("Description=" + peer.getUsername());
+			});
+
+			return Optional.of(netdev);
+		}
+
+		@Override
+		public Optional<FileUnit> getNetworkFile() {
+			return Optional.empty(); //No network file here
+		}
+
+		public void addWireGuardPeer(UserModel user) {
+			if (this.peers == null) {
+				this.peers = new ArrayList<>();
+			}
+
+			this.peers.add(user);
+		}
+	}
+
+	private class WireGuardConfig extends AData {
+		public WireGuardConfig(JsonObject wireguardData) throws ADataException {
+			super("wireguard", null, wireguardData);
+		}
+
+		/**
+		 * Port WireGuard should be listening on
+		 */
+		public int getListenPort() {
+			return getData().getInt("listen_port", 51820);
+		}
+
+		@Override
+		public AData read(JsonObject data) throws ADataException {
+			return null;
+		}
+	}
+
+	private WireGuardConfig data;
 
 	public WireGuard(ServerModel me) {
 		super(me);
+	}
 
-		final JsonObject wgSettings = getServerModel().getData().getData().getJsonObject("wireguard");
-		this.listenPort = wgSettings.getInt("listen_port", 51820);
+	private WireGuard.WireGuardConfig getMyConfig() {
+		if (null == this.data) {
+			try {
+				this.data = new WireGuardConfig(getServerModel().getData().getData());
+			} catch (ADataException e) {
+				e.printStackTrace();
+			}
+		}
+		return this.data;
 	}
 
 	@Override
@@ -58,9 +146,11 @@ public class WireGuard extends AStructuredProfile {
 
 		WireGuardModel nic = null;
 		try {
-			nic = new WireGuardModel(getNetworkModel());
+			nic = new WireGuardModel(new NetworkInterfaceData("wireguard", null, Json.createObjectBuilder().build()), getNetworkModel());
 		} catch (InvalidNetworkInterfaceException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ADataException e) {
 			e.printStackTrace();
 		}
 
@@ -68,7 +158,7 @@ public class WireGuard extends AStructuredProfile {
 			nic.addWireGuardPeer(user);
 		}
 
-		nic.setListenPort(this.listenPort);
+		nic.setListenPort(getMyConfig().getListenPort());
 		//TODO
 		//nic.addAddress(getNetworkModel().getSubnet(MachineType.VPN));
 
@@ -86,7 +176,7 @@ public class WireGuard extends AStructuredProfile {
 	public final Collection<IUnit> getPersistentFirewall() throws InvalidPortException {
 		final Collection<IUnit> units = new ArrayList<>();
 
-		getServerModel().addListen(Encapsulation.UDP, this.listenPort);
+		getServerModel().addListen(Encapsulation.UDP, getMyConfig().getListenPort());
 
 		return units;
 	}
